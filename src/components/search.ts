a9c79ded303d6199a5e7240d6aeada15692e4e94
Adam <adam.alani@epita.fr>
2021-01-29 01:32:44 +0100

    fixed link

--- src/components/search.ts
+++ src/components/search.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default function loadOptions(filterText) {
     if (filterText.length >= 3) {
-        return axios({ method: "POST", url: "http://localhost:8090/", data: {school: filterText }, headers: {"content-type": "text/plain"}})
+        return axios({ method: "POST", url: "https://parcourstupback.herokuapp.com/", data: {school: filterText }, headers: {"content-type": "text/plain"}})
             .then(result => result.data)
     }
 }
