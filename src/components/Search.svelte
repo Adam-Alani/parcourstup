0abca3d4093d20eb79677f260d6e0f75d2119bbc
Adam <adam.alani@epita.fr>
2021-02-23 15:01:35 +0100

    more...

--- src/components/Search.svelte
+++ src/components/Search.svelte
@@ -17,12 +17,12 @@
 </script>
 <Tailwindcss />
 <div class="bg-pblue my-10 flex md:flex-row flex-col">
-    <section class="py-12 flex-1">
+    <section class="md:py-12 py-2 flex-1">
         <div class=" py-2 px-2 ml-12 w-72  text-sm  focus:outline-none themed">
             <Select {loadOptions} {optionIdentifier} {getSelectionLabel} {Item}  placeholder="Cherchez vos Formations"></Select>
         </div>
     </section>
-    <aside class="flex flex-wrap flex-row justify-start flex-1 my-2 ">
+    <aside class="flex flex-wrap flex-row justify-start items-center justify-center flex-1 my-2 ">
         {#each $schools as schoolName}
             <Chips name={schoolName["School"]}/>
         {/each}
